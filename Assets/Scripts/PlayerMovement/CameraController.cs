e3c922fb2ea1d1181abf1edf8f7ab9deba63b822
Phoe <darius.san.mobile@gmail.com>
2023-07-19 22:58:46 +0200

    Joined all scenes in one, added teleports and fixed the camera

--- Assets/Scripts/PlayerMovement/CameraController.cs
+++ Assets/Scripts/PlayerMovement/CameraController.cs
@@ -1,20 +1,58 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject player;
-    private float relativePos;
+    [SerializeField] private List<Area> areas = new List<Area> ();
+    [SerializeField]private Area currentArea;
+
+    [SerializeField] private GameObject player;
+
+    private Vector3 vectorTransform;
+    private float relativePosX;
+    private float relativePosZ;
+    
 
     void Start()
     {
-        relativePos = transform.position.x - player.transform.position.x;
+        currentArea = areas[0];
+
+        transform.position = currentArea.GetStartingPos();
+
+        relativePosX = transform.position.x - player.transform.position.x;
+        relativePosZ = transform.position.z - player.transform.position.z;
     }
 
     void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x + relativePos, transform.position.y, transform.position.z);
+        if (!currentArea.isCurrentArea) 
+        {
+            SeachAreaAndStart();
+        }
+        
+        float coordX = currentArea.IsXFixed() * (transform.position.x) + (1 - currentArea.IsXFixed()) * (player.transform.position.x + relativePosX);
+        float coordZ = currentArea.IsZFixed() * (transform.position.z) + (1 - currentArea.IsZFixed()) * (player.transform.position.z + relativePosZ);
+        transform.position = new Vector3(coordX, transform.position.y, coordZ);
+    }
+
+    private void SeachAreaAndStart()
+    {
+        for (int i = 0; i < areas.Count; i++)
+        {
+            if (areas[i].isCurrentArea)
+            {
+                currentArea = areas[i];
+                break;
+            }
+        }
+
+        transform.position = currentArea.GetStartingPos();
+
+        relativePosX = transform.position.x - player.transform.position.x;
+        relativePosZ = transform.position.z - player.transform.position.z;
     }
 
 }
