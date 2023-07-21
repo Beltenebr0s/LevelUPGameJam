e51c12de88f43a43b930fb72aab326f6d4e02433
CharlieM97 <98742130+Cmomnogmgg@users.noreply.github.com>
2023-07-21 19:27:10 +0200

    Algo de la UI IOKESE
    Ya no me da el riego para acordarme de todo lo que he hecho en este commit

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
@@ -48,7 +49,7 @@ public class MenuController : MonoBehaviour
 
     public void StartGame()
     {
-        //Game starts
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
